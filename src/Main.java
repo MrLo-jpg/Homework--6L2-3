5fb1fbe60b0f0be8b9c06dde2907ff9b2a436745
sergejlodkin <lsa-71@mail.ru>
2024-09-24 21:49:47 +0300

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) {//Задача 3
         ;
         System.out.println(calculateDeliveryDays(80));
     }
